f516cc9518252afe0b410bf730c556d7369faa46
Chaps <62523886+ElGranV@users.noreply.github.com>
2020-08-23 14:14:27 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let div = document.querySelector('div');
-let connect_html = "<img src = 'logo.png' id = 'logo'> "
-connect_html += '<input type="text" id = "input_identif" class = "inputs"> <input type="password" id = "input_mdp" class = "inputs">';
-let connect_button = document.getElementById("connect");
-connect_button.addEventListener("click", ()=>{
-    div.innerHTML = connect_html;
-})
